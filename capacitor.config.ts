afb5499b70989298337f02bef4e3264db8d27e99
mmasser95 <mmasser95@gmail.com>
2024-03-03 15:52:38 +0100

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TFG-Client',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <ion-app>
+    <ion-page>
+      <ion-tabs>
+        <ion-router-outlet />
+	<ion-tab-bar slot="bottom">
+          <ion-tab-button tab="home" href="/home">
+            <ion-icon :icon="playCircle" />
+            <ion-label>Listen now</ion-label>
+          </ion-tab-button>
+          <ion-tab-button tab="radio" href="/radio">
+            <ion-icon :icon="radio" />
+            <ion-label>Radio</ion-label>
+          </ion-tab-button>
+          <ion-tab-button tab="library" href="/library">
+            <ion-icon :icon="library" />
+	    <ion-label>Library</ion-label>
+	  </ion-tab-button>
+          <ion-tab-button tab="search" href="/search">
+            <ion-icon :icon="search" />
+            <ion-label>Search</ion-label>
+          </ion-tab-button>
+        </ion-tab-bar>
+      </ion-tabs>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script lang="js">
+import { IonApp, IonRouterOutlet, IonPage, IonTabs, IonTabBar, IonTabButton, IonLabel, IonIcon } from '@ionic/vue';
+import { playCircle, radio, library, search } from 'ionicons/icons';
+
+export default{
+	components:{IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonLabel, IonIcon,IonApp},
+	data(){
+		return {
+			playCircle,
+			radio,
+			library,
+			search
+		}
+	},
+}
+</script>
